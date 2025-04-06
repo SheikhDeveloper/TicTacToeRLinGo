5ceed8e3555598c74c1a057ab969af5dcb3e4a14
Sheikh <tvemperror00@gmail.com>
2025-04-06 19:18:17 +0300

    Implement TTT model with RL

--- ttt-rl.go
+++ ttt-rl.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
-	"time"
+	"strconv"
 )
 
 type GameState struct {
@@ -36,6 +36,10 @@ func newNeuralNetwork(inputSize, outputSize, hiddenSize int) *NeuralNetwork {
 		weightsHO: make([]float64, hiddenSize*outputSize),
 		biasesH: make([]float64, hiddenSize),
 		biasesO: make([]float64, outputSize),
+		inputs: make([]float64, inputSize),
+		hidden: make([]float64, hiddenSize),
+		outputs: make([]float64, outputSize),
+		rawLogits: make([]float64, outputSize),
 	}
 }
 
@@ -61,7 +65,6 @@ func reLUDerivative(x float64) float64 {
 }
 
 func initNN(inputSize, outputSize, hiddenSize int) *NeuralNetwork {
-	rand.Seed(time.Now().UnixNano())
 
 	nn := newNeuralNetwork(inputSize, outputSize, hiddenSize)
 	for i := 0; i < nn.inputSize*nn.hiddenSize; i++ {
@@ -80,19 +83,17 @@ func initNN(inputSize, outputSize, hiddenSize int) *NeuralNetwork {
 }
 
 func softmax(input, output *[]float64) {
-	var max float64
-	max = (*input)[0]
+	var maximum float64
+	maximum = (*input)[0]
 	for _, v := range *input {
-		if v > max {
-			max = v
+		if v > maximum {
+			maximum = v
 		}
 	}
-	for i, v := range *input {
-		(*output)[i] = math.Exp(v - max)
-	}
 	var sum float64
-	for _, v := range *output {
-		sum += v
+	for i, _ := range *output {
+		(*output)[i] = math.Exp((*input)[i] - maximum)
+		sum += (*output)[i]
 	}
 	if sum > 0 {
 		for i, _ := range *output {
@@ -128,11 +129,9 @@ func (nn *NeuralNetwork) forwardPass(input []float64) {
 	softmax(&nn.rawLogits, &nn.outputs)
 }
 
-func initGame() *GameState {
-	return &GameState{
-		board: [9]string{".", ".", ".", ".", ".", ".", ".", ".", "."},
-		currentPlayer: 0,
-	}	
+func (gs *GameState) initGame() {
+	gs.currentPlayer = 0
+	gs.board = [9]string{"." , "." , "." , "." , "." , "." , "." , "." , "."}
 } 
 
 func displayBoard(board [9]string) {
@@ -141,13 +140,17 @@ func displayBoard(board [9]string) {
 	fmt.Println(board[6], board[7], board[8])
 }
 
-func (gs *GameState) boardToInputs() []float64 {
-	inputs := make([]float64, 9)
+func (gs *GameState) boardToInputs(inputs []float64) []float64 {
 	for i := 0; i < 9; i++ {
-		if gs.board[i] == "X" {
-			inputs[i] = 1
-		} else if gs.board[i] == "O" {
-			inputs[i] = -1
+		if gs.board[i] == "." {
+			inputs[i * 2] = 0
+			inputs[i * 2 + 1] = 0
+		} else if gs.board[i] == "X" {
+			inputs[i * 2] = 1
+			inputs[i * 2 + 1] = 0
+		} else {
+			inputs[i * 2] = 0
+			inputs[i * 2 + 1] = 1
 		}
 	}
 	return inputs
@@ -155,27 +158,24 @@ func (gs *GameState) boardToInputs() []float64 {
 
 func (gs *GameState) checkGameOver(winner *string) bool {
 	for i := 0; i < 3; i++ {
-		if gs.board[i*3] == gs.board[i*3+1] && gs.board[i*3] == gs.board[i*3+2] && gs.board[i*3] != "." {
+		if gs.board[i*3] != "." && gs.board[i*3] == gs.board[i*3+1] && gs.board[i*3+1] == gs.board[i*3+2] {
 			*winner = gs.board[i*3]
-			return true
-		}
-		if gs.board[i] == gs.board[i+3] && gs.board[i] == gs.board[i+6] && gs.board[i] != "." {
-			*winner = gs.board[i]
 			return true
 		}
 	}
 
 	for i := 0; i < 3; i++ {
-		if gs.board[i] == gs.board[i+3] && gs.board[i] == gs.board[i+6] && gs.board[i] != "." {
+		if gs.board[i] == gs.board[i+3] && gs.board[i+3] == gs.board[i+6] && gs.board[i] != "." {
+			*winner = gs.board[i]
 			return true
 		}
 	}
 
-	if gs.board[0] == gs.board[4] && gs.board[0] == gs.board[8] && gs.board[0] != "." {
+	if gs.board[0] == gs.board[4] && gs.board[4] == gs.board[8] && gs.board[0] != "." {
 		*winner = gs.board[0]
 		return true
 	}
-	if gs.board[2] == gs.board[4] && gs.board[2] == gs.board[6] && gs.board[2] != "." {
+	if gs.board[2] == gs.board[4] && gs.board[4] == gs.board[6] && gs.board[2] != "." {
 		*winner = gs.board[2]
 		return true
 	}
@@ -196,18 +196,17 @@ func (gs *GameState) checkGameOver(winner *string) bool {
 
 func getComputerMove(gs *GameState, nn *NeuralNetwork, displayProbas bool) int {
 
-	inputs := gs.boardToInputs()
+	inputs := make([]float64, nn.inputSize)
+	inputs = gs.boardToInputs(inputs)
 	nn.forwardPass(inputs)
 
 	maxProba := -1.0
-	maxProbaIdx := -1
 	bestMove := -1
 	bestLegalProba := -1.0
 
 	for i, proba := range nn.outputs {
 		if proba > maxProba {
 			maxProba = proba
-			maxProbaIdx = i
 		}
 
 		if gs.board[i] == "." {
@@ -230,7 +229,7 @@ func getComputerMove(gs *GameState, nn *NeuralNetwork, displayProbas bool) int {
 		fmt.Println(totalProba)
 	}
 
-	return maxProbaIdx
+	return bestMove
 }
 
 func (nn *NeuralNetwork) backwardPass(target_probas []float64, lr float64, rewardScale float64) {
@@ -269,7 +268,7 @@ func (nn *NeuralNetwork) backwardPass(target_probas []float64, lr float64, rewar
 	}
 }
 
-func learnFromGame(nn *NeuralNetwork, moveHistory []int, numMoves int, nnMovesEven bool, winner string) {
+func (nn *NeuralNetwork) learnFromGame(moveHistory []int, numMoves int, nnMovesEven bool, winner string) {
 	var reward float64
 	var nnSymbol string
 	if nnMovesEven {
@@ -289,5 +288,202 @@ func learnFromGame(nn *NeuralNetwork, moveHistory []int, numMoves int, nnMovesEv
 	var gs GameState
 	targetProbas := make([]float64, nn.outputSize)
 
+	for moveIdx := 0; moveIdx < numMoves; moveIdx++ {
+		if (nnMovesEven && moveIdx%2 == 0) || (!nnMovesEven && moveIdx%2 == 1) {
+			continue
+		}
 
+		gs.initGame()
+		for i := 0; i < moveIdx; i++ {
+			var symbol string 
+			if i % 2 == 0 {
+				symbol = "X"
+			} else {
+				symbol = "O"
+			}
+			gs.board[moveHistory[i]] = symbol
+		}
+
+		inputs := make([]float64, nn.inputSize)
+		inputs = gs.boardToInputs(inputs)
+
+		nn.forwardPass(inputs)
+
+		move := moveHistory[moveIdx]
+
+		move_importance := 0.5 + 0.5 * float64(moveIdx) / float64(numMoves)
+		scaledReward := reward * move_importance
+
+		for i := 0; i < nn.outputSize; i++ {
+			targetProbas[i] = 0
+		}
+
+		if scaledReward > 0 {
+			targetProbas[move] = 1.0
+		} else {
+			validMovesLeft := 9 - moveIdx - 1
+			otherProba := 1.0 / float64(validMovesLeft)
+			for i := 0; i < 9; i++ {
+				if gs.board[i] == "." && i != move {
+					targetProbas[i] = otherProba
+				}
+			}
+		}
+
+		fmt.Println(scaledReward)
+		nn.backwardPass(targetProbas, 0.1, scaledReward)
+	}
+}
+
+func (nn *NeuralNetwork) playGame() {
+	var gs GameState
+	var winner string
+	moveHistory := make([]int, 9)
+	var numMoves int
+
+	gs.initGame()
+
+	fmt.Println("Welcome to Tic Tac Toe! You are X, the computer is O.")
+	fmt.Println("Enter positions as numbers from 0 to 8 (see picture).")
+
+	for gameOver := gs.checkGameOver(&winner); !gameOver; gameOver = gs.checkGameOver(&winner) {
+		displayBoard(gs.board)
+
+		if gs.currentPlayer == 0 {
+			
+			var move int
+			var movec string
+			fmt.Println("Your move (0-8): ")
+
+			fmt.Scan(&movec)
+			move, _ = strconv.Atoi(movec)
+
+			if move < 0 || move > 8 || gs.board[move] != "." {
+				fmt.Println("Invalid move! Try again.")
+				continue
+			}
+
+			gs.board[move] = "X"
+			moveHistory[numMoves] = move
+			numMoves++
+		} else {
+			fmt.Println("Computer's move:")
+			move := getComputerMove(&gs, nn, true)
+			gs.board[move] = "O"
+			fmt.Println("Computer placed O in position", move)
+			moveHistory[numMoves] = move
+			numMoves++
+		}
+
+		gs.currentPlayer = gs.currentPlayer ^ 1
+	}
+
+	displayBoard(gs.board)
+	if winner == "draw" {
+		fmt.Println("Draw!")
+	} else if winner == "X" {
+		fmt.Println("You win!")
+	} else {
+		fmt.Println("Computer wins!")
+	}
+
+	nn.learnFromGame(moveHistory, numMoves, true, winner)
+}
+
+func (gs *GameState) getRandomMove() int {
+	for {
+		move := rand.Intn(9)
+		if gs.board[move] != "." {
+			continue
+		}
+		return move
+	}
+}
+
+func (nn NeuralNetwork) playRandomGame(moveHistory []int, numMoves *int) string {
+	var gs GameState
+	var winner string
+	*numMoves = 0
+
+	gs.initGame()
+
+	for ; !gs.checkGameOver(&winner); {
+		var move int
+
+		if gs.currentPlayer == 0 {
+			move = gs.getRandomMove()
+		} else {
+			move = getComputerMove(&gs, &nn, false)
+		}
+
+		var symbol string
+		if gs.currentPlayer == 0 {
+			symbol = "X"
+		} else {
+			symbol = "O"
+		}
+		gs.board[move] = symbol
+		moveHistory[*numMoves] = move
+		*numMoves++
+
+		gs.currentPlayer = gs.currentPlayer ^ 1
+	}
+
+	nn.learnFromGame(moveHistory, *numMoves, true, winner)
+	return winner
+}
+
+func (nn NeuralNetwork) trainAgainstRandom(numGames int) {
+	moveHistory := make([]int, 9)
+	numMoves := 0
+	wins := 0
+	losses := 0
+	ties := 0
+
+	fmt.Printf("Training NN against %d random games...\n", numGames)
+
+	playedGames := 0
+	for i := 0; i < numGames; i++ {
+		winner := nn.playRandomGame(moveHistory, &numMoves)
+		playedGames++
+
+		if winner == "draw" {
+			ties++
+		} else if winner == "O" {
+			wins++
+		} else {
+			losses++
+		}
+
+		if i + 1 % 10000 == 0 {
+			fmt.Printf("Played %d games. Wins: %.2f, Losses: %.2f, Ties: %.2f\n", playedGames, float64(wins) / float64(playedGames) * 100, 
+			float64(losses) / float64(playedGames) * 100, float64(ties) / float64(playedGames) * 100)
+			wins = 0
+			losses = 0
+			ties = 0
+			playedGames = 0
+		}
+	}
+
+	fmt.Println("Training complete!")
+}
+
+func main() {
+
+	randomGames := 150000
+
+	nn := initNN(18, 9, 100)
+
+	if randomGames > 0 { nn.trainAgainstRandom(randomGames) }
+
+	for {
+		var playAgain string
+		nn.playGame()
+
+		fmt.Println("Play again? (y/n)")
+		fmt.Scan(&playAgain)
+		if playAgain != "y" && playAgain != "Y" {
+			break
+		}
+	}
 }
